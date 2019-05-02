03545b0ce4b320203fac4a789fd241fb6fa584b3
Squirtachu <48461781+Squirtachu@users.noreply.github.com>
2019-05-01 20:45:51 -0600

    Add files via upload

---
+++ Credit_Tracker_1.py
@@ -0,0 +1,32 @@
+# Credit Tracker Software
+
+# Start Program
+
+# is this a new file?
+
+# Yes new file - create a new file
+
+#   get Card Name
+#   Get Int Rate
+#   Get Start Date
+#   Get Start Balance
+#
+#   Check if file already exists
+#   YES : get a new name
+#   NO: Create the file
+#
+# NO not a new file
+#   get card name
+#   Open/retrieve the data file
+#
+# display card data
+# display file data
+# Entering new data?
+# YES:
+#   Get txnDate
+#   Get Txn Type
+#   Get Txn Amount
+#   Get txn Description
+#   process the new txn and calculate Acc int
+#       Acc int = blance * int rate /365 * number of days between days
+#   If txn type is int post the acc int and get user to confirm/correct amount
